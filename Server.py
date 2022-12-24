4abcaaf9bb84f6ac34a1a51bf03907066b118809
Neetigya Verma <76085539+Wizz2206@users.noreply.github.com>
2022-12-24 17:23:05 +0530

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,6 +1,3 @@
-# Future Aim :- Multithreading and achieve live connection between all 3 Slave nodes
-# We have made a distributed system calculator to visualize the improved approach to 3PC algorithm which we came up with. Here Transaction manager is our server node which distributes the workload among the Client Nodes which are supposed to be Participating Nodes.
-
 import socket
 import time
 
